215f686ec924b760c80cd961bc7956db6fd37bc5
Jan Freyberg <jan.freyberg@gmail.com>
2016-11-18 21:10:21 +0000

    UI Improvements, Step Functionality

--- server.R
+++ server.R
@@ -6,21 +6,18 @@
 #
 
 library(shiny)
+library(sn)
 
-intervals <- rep(list(c(0, 0)), 100)
+intervals <- rep(list(c(0, 0)), 40)
 barheight <- rep(0, 500)
+barnorm <- barheight
+x <- barheight
 cumulatives <- 1
 speed <- 1.0
 confidence <- 0.95
 stepping <- FALSE
 
 shinyServer(function(input, output, session) {
-  
-  
-  observeEvent(input$speed,
-                {
-                  speed <<- input$speed
-                })
   
   observeEvent(input$stepButton, {
     if(!input$running){
@@ -31,18 +28,18 @@ shinyServer(function(input, output, session) {
     stepping <<- TRUE
     }
   })
-
+  
+  # If the input changes, reset intervals
+  observeEvent(input$mean | input$stdev | input$nsamp | input$conf.level | input$resetButton, {
+    confidence <<- input$conf.level
+    intervals <<- rep(list(c(0, 0)), 100)
+    barheight <<- rep(0, 500)
+    cumulatives <<- 1
+  })
+  
   
   output$conf.plot<-renderPlot({
-    # Make sure this plot updates regularly
-    # autoRefresh()
-    
-    if(input$running){
-      xstring = 'Running'
-    } else {
-      xstring = 'Paused'
-    }
-    
+
     plot(NULL
          ,xlim = c(input$mean-input$stdev,input$mean+input$stdev)
          ,ylim = c(0,40)
@@ -52,8 +49,7 @@ shinyServer(function(input, output, session) {
          ,main="CIs of lots of studies over time (red CIs do not include the mean)"
          ,cex.main=1.5
          ,cex.lab=1.5
-         ,xlab = xstring
-    )
+         ,xlab = NULL)
     
     axis(1,
          at = c(input$mean-input$stdev, input$mean-0.5*input$stdev,
@@ -67,36 +63,37 @@ shinyServer(function(input, output, session) {
          cex.axis=2,
          yaxt='n')
     
-    # Create a new interval
-    x <<- rnorm(input$nsamp, mean = input$mean, sd = input$stdev)
-    test <- t.test(x,conf.level=input$conf.level)
-    
-    intervals <<- c(list(test$conf.int), intervals) # feed at bottom
-    intervals[[101]] <<- NULL # cut off top
-    
-    # update histogram
-    barheight <<- barheight + ((seq(input$mean-input$stdev,
-                                   input$mean+input$stdev,
-                                   length.out=500) > intervals[[1]][1]) &
-                               (seq(input$mean-input$stdev,
-                                    input$mean+input$stdev,
-                                    length.out=500) < intervals[[1]][2]))
-    # normalise the histogram
-    cumulatives <<- cumulatives + 1
-    barnorm <<- barheight / cumulatives
-
     # Plot the intervals
     for (i in 1:40){
       interval = intervals[[i]]
       if(input$mean>interval[1] & input$mean<interval[2]){
-        lines(c(interval[1],interval[2]),c(i,i), lwd=2,col='black')
-      }
-      else{
-        lines(c(interval[1],interval[2]),c(i,i), lwd=2,col='red' )
-      }
+        lineColor <- 'black' }
+      else {
+        lineColor <- 'red' }
+      lines(c(interval[1],interval[2]),c(i,i), lwd=2,col=lineColor)
     }
     
     if(input$running){
+      # Create a new Interval
+      
+      x <<- rnorm(input$nsamp, mean = input$mean, sd = input$stdev)
+      
+      test <- t.test(x,conf.level=input$conf.level)
+      intervals <<- c(list(test$conf.int), intervals) # feed at bottom
+      intervals[[41]] <<- NULL # cut off top
+
+      # Update histogram
+      barheight <<- barheight + ((seq(input$mean-input$stdev,
+                                      input$mean+input$stdev,
+                                      length.out=500) > intervals[[1]][1]) &
+                                   (seq(input$mean-input$stdev,
+                                        input$mean+input$stdev,
+                                        length.out=500) < intervals[[1]][2]))
+      # Normalise the histogram
+      cumulatives <<- cumulatives + 1
+      barnorm <<- barheight / cumulatives
+      
+      # Auto-Update the Plot
       if(stepping){
         updateCheckboxInput(session,
                             inputId = "running",
@@ -104,10 +101,9 @@ shinyServer(function(input, output, session) {
                             value = FALSE)
         stepping <<- FALSE
       } else {
-        invalidateLater(500 / speed)
+        invalidateLater(500 / input$speed)
       }
     }
-
   })
   
   output$hist.plot <- renderPlot({
@@ -139,7 +135,7 @@ shinyServer(function(input, output, session) {
                             value = FALSE)
         stepping <<- FALSE
       } else {
-        invalidateLater(500 / speed)
+        invalidateLater(500 / input$speed)
       }
     }
   })
@@ -154,21 +150,23 @@ shinyServer(function(input, output, session) {
                             value = FALSE)
         stepping <<- FALSE
       } else {
-        invalidateLater(500 / speed)
+        invalidateLater(500 / input$speed)
       }
     }
     return(
-      paste(" Population Mean: ", input$mean, "\n",
+      paste(" Population Mean: ", input$mean, " | ",
       "Sample Mean: ", round(mean(x), digits=2), "\n",
-      "Confidence Interval: \n",
+      "Sample St. Dev:  ", round(sd(x), digits=2), "\n",
+      "Confidence Interval: ",
       round(intervals[[1]][1], digits=2), " - ",
       round(intervals[[1]][2], digits=2))
     )
   })
+  
   # Small histogram of sample for left hand side:
   output$samp.hist <- renderPlot({
     if(input$running){
-      invalidateLater(500 / speed)
+      invalidateLater(500 / input$speed)
       if(stepping){
         updateCheckboxInput(session,
                             inputId = "running",
@@ -177,13 +175,17 @@ shinyServer(function(input, output, session) {
         stepping <<- FALSE
       }
     }
+    # Plot histogram of sample
     hist(x,
          xlim = c(input$mean-2*input$stdev, input$mean+2*input$stdev),
-         breaks=100,
-         main="Histogram of Sample",
+         ylim = c(0, 0.4),
+         breaks=50,
+         freq=FALSE,
+         main="Histogram of Sample and Distribution of Population",
          yaxt='n',
          xaxt='n',
          axes=FALSE)
+    # Scale anbd label the axes
     axis(1,
          at = c(input$mean-input$stdev, input$mean-0.5*input$stdev,
                 input$mean,
@@ -197,13 +199,6 @@ shinyServer(function(input, output, session) {
          yaxt='n')
   })
   
-  # If the input changes, reset intervals
-  observeEvent(input$mean | input$stdev | input$nsamp | input$conf.level | input$resetButton, {
-    confidence <<- input$conf.level
-    intervals <<- rep(list(c(0, 0)), 100)
-    barheight <<- rep(0, 500)
-    cumulatives <<- 1
-  })
   
 
 
--- ui.R
+++ ui.R
@@ -11,46 +11,40 @@ shinyUI(fluidPage(
 
   # Application title
   titlePanel("Confidence Interval Simulation"),
-
-  # Sidebar with a slider input for number of bins
+  p(paste("This simulation 'runs' a large number of studies over time by simulating ",
+          "a random sample from a normal distribution. The confidence intervals that ",
+          "result from this are shown in the top plot. Red intervals do not include ",
+          "the true population mean. Additionally, a cumulative histogram of the ",
+          "proportion of times a value was inside the confidence interval is shown ",
+          "on the bottom. This value will tend towards the confidence level for the ",
+          "population mean.")),
+  # Sidebar with input
   sidebarLayout(
     sidebarPanel(
-      numericInput(inputId = "nsamp"
-                   ,label=strong("Sample Size")
-                   ,value=100
-                   ,min=5
-                   ,max=10000)
-      ,numericInput(inputId = "mean"
-                    ,label = strong("Mean")
-                    ,value = 5)
-      ,numericInput(inputId = "stdev"
-                    ,label = strong("Standard Deviation")
-                    ,value = 2
-                    ,min=.01
-                    ,max=100)
-      
-      ,numericInput(inputId = "conf.level"
-                    ,label=strong("Confidence Level")
-                    ,value=0.95
-                    ,min=0.1
-                    ,max=0.99
-                    ,step=0.04)
-      ,sliderInput(inputId = "speed",
+      fluidRow(column(6, numericInput(inputId = "nsamp",
+                                      label=strong("Sample Size"),
+                                      value=100, min=5, max=10000)),
+               column(6, numericInput(inputId = "mean",
+                                      label = strong("Mean"),
+                                      value = 5))),
+      fluidRow(column(6, numericInput(inputId = "stdev",
+                                      label = strong("Standard Deviation"),
+                                      value = 2, min=.01, max=100)),
+               column(6, numericInput(inputId = "conf.level",
+                                      label=strong("Confidence Level"),
+                                      value=0.95, min=0.1, max=0.99, step=0.04))),
+      sliderInput(inputId = "speed",
                     label=strong("Animation Speed"),
-                    value=5.0, min=0.1, max=10, step=0.1)
-      ,checkboxInput(inputId = "running",
-                     label='Run Simulation',
-                     value = FALSE)
-      ,actionButton(inputId = 'resetButton',
-                    label="Reset")
-      ,actionButton(inputId = 'stepButton',
-                    label = 'Step')
+                    value=1.0, min=0.1, max=10, step=0.1),
+      checkboxInput(inputId = "running", label='Run Simulation', value = FALSE),
+      actionButton(inputId = 'resetButton', label="Reset Animation"),
+      actionButton(inputId = 'stepButton', label = 'Generate Sample'),
+      hr(),
       # Sample properties and histogram
-      ,verbatimTextOutput('sample')
-      ,plotOutput('samp.hist', height='200px')
+      verbatimTextOutput('sample'),
+      plotOutput('samp.hist', height='200px')
     ),
-
-    # Show a plot of the generated distribution
+    
     mainPanel(plotOutput('conf.plot', height="300px"),
               plotOutput('hist.plot', height="400px"))
   )
